c1bd3a7a84af5b76b337b539e280057e1e37634c
haobo12 <96019098+haobo12@users.noreply.github.com>
2024-12-05 20:55:18 +0100

    uwunya

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,11 +2,12 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Buzz’s Blog',
-    description: 'A humble Astronaut’s guide to the stars',
+    title: 'astrid’s Blog',
+    description: 'a bunch of random stuff i like to write about',
     site: context.site,
     items: await pagesGlobToRssItems(
       import.meta.glob('./blogs/*.md'),
     ),
+	stylesheet: '/pretty-feed-v3.xsl',
   });
 }
