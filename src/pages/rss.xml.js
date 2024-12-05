38a32d3e9d4c494335ed05c6d78dc8415e1053a5
haobo12 <96019098+haobo12@users.noreply.github.com>
2024-12-05 20:49:09 +0100

    fucking hell

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,12 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
-rss({
-	stylesheet: '/pretty-feed-v3.xsl',
-});
+
 export async function GET(context) {
   return rss({
-    title: 'astrid\'s blog',
-    description: 'a bunch of random stuff i like to write bout',
+    title: 'Buzz’s Blog',
+    description: 'A humble Astronaut’s guide to the stars',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./blogs/*.md')),
-    customData: `<language>en-GB</language>`,
+    items: await pagesGlobToRssItems(
+      import.meta.glob('./blogs/*.md'),
+    ),
   });
 }
